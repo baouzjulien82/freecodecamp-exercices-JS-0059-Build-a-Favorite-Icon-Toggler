91fb7349ac2ba2d30ba5889a58c338d2e4543933
Julien Baouz <baouzjulien@gmail.com>
2025-11-04 21:32:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const buttons = document.querySelectorAll('favorite-icon');
+
+const filledOrNot = (btn) => {
+
+};
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    filledOrNot(btn);
+  });
+});
